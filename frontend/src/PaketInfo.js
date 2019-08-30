3bfd34e46290ea271c4879a3ce0e17db6761a047
Satryaji Aulia <satraul@macports.org>
2019-08-30 17:18:11 +0700

    WIP: Init frontend

---
+++ frontend/src/PaketInfo.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Mascot from './assets/mascot.png';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Button } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    '@global': {
+        body: {
+            backgroundColor: theme.palette.common.white,
+        },
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        // flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+function PaketInfo(props) {
+    const classes = useStyles();
+    const slug = props.match.params.slug;
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <div className={classes.paper}>
+                        <div className={classes.titles}>
+                            <Typography component="h1" variant="h5">
+                                Paket {slug}
+                            </Typography>
+                            <Typography>
+                                Ini dia info untuk paket {slug}.
+                            </Typography>
+                        </div>
+                        <img width="15%" src={Mascot} alt="Mascot" />
+                    </div>
+                    <ExpansionPanel defaultExpanded>
+                        <ExpansionPanelSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography className={classes.heading}>Kewajiban</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                sit amet blandit leo lobortis eget.
+                            </Typography>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                    <ExpansionPanel defaultExpanded>
+                        <ExpansionPanelSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2a-content"
+                            id="panel2a-header"
+                        >
+                            <Typography className={classes.heading}>Hak</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                sit amet blandit leo lobortis eget.
+                            </Typography>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        href="/paket"
+                    >
+                        Mantap Bos
+                    </Button>
+                </Container>
+            </header>
+        </div >
+    );
+}
+
+export default PaketInfo;
